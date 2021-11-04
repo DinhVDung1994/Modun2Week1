c26300550fce0485947e2b27e63cd0851e4116ec
unknown <dinhdung02031994@gmai.com>
2021-11-04 16:36:37 +0700

    abc

---
+++ src/dayofmonth.java
@@ -0,0 +1,34 @@
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
+public class dayofmonth {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Which month that you want to count dáy?");
+        int month = scanner.nextInt();
+        switch (month){
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println("The month "+month+"has 31 dáy!");
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println("The month "+month+"has 30 days!");
+                break;
+            case 2:
+                System.out.println("The month "+month+"has 28 or29 days!");
+                break;
+            default:
+                System.out.println("Invalid input!");
+                break;
+        }
+    }
+}
